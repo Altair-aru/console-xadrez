943c560815ce8f3f02a97f6656419615dbaecc62
altairjr <altair.gcjr@gmail.com>
2017-12-14 00:02:45 -0200

    Incluindo peças e logica de exceção do tabuleiro

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -12,14 +12,21 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiro tab = new Tabuleiro(8, 8);
 
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+            try { 
+                    Tabuleiro tab = new Tabuleiro(8, 8);
 
-            Tela.imprimirTabuleiro(tab);
+                    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
+                    Tela.imprimirTabuleiro(tab);            
+                 }
+
+            catch (TabuleiroExceptions e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
