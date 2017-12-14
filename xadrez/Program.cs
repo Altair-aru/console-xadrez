afb2f0e00d1abdf88dcb2284881c293254c2b69d
altairjr <altair.gcjr@gmail.com>
2017-12-13 22:11:34 -0200

    Iniciado o tabuleiro - ok

--- xadrez/Program.cs
+++ xadrez/Program.cs
@@ -11,10 +11,9 @@ namespace xadrez
     {
         static void Main(string[] args)
         {
-            Posicao P;
-            P = new Posicao(3, 4);
+            Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
 
-            Console.WriteLine("Posição: " + P);
             Console.ReadLine();
         }
     }
